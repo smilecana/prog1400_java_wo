86f635b640e6646e0f590599ccad0cb67d718e12
kh3996 <w0419410@nscc.ca>
2019-01-07 09:48:51 -0400

    Finished Car demo

---
+++ Labs/Lab1/src/ca/prog1400/Car.java
@@ -0,0 +1,15 @@
+package ca.prog1400;
+
+public class Car {
+    // Everything to do with "Car" goes here
+    double mpg;
+    double price = 20000.99;
+    String make = "Toyota";
+    boolean isInsured;
+
+    public void setPrice(double newPrice) {
+        // Set the price
+        this.price = newPrice;
+    }
+}
+
--- Labs/Lab1/src/ca/prog1400/Main.java
+++ Labs/Lab1/src/ca/prog1400/Main.java
@@ -3,6 +3,14 @@ package ca.prog1400;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Prog 1400!!!");
+        String courseTitle = "Prog 1400!!!";
+        boolean isRaining = false;
+        int temp = -5;
+        int foo;
+        float hst;
+        double gst = 0.08;
+        char x = 'x';
+
+        System.out.println(courseTitle);
     }
 }
